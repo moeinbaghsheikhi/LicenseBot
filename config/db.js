20a46cbf91c6eea7add721388949fb306378e019
moein <moein.baghsheikhi@gmail.com>
2024-06-19 21:04:37 +0330

    start and login admin

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const knex = require('knex')({
       port: 3306,
       user: 'root',
       password: '',
-      database: 'gpt',
+      database: 'license',
     },
 });
 
--- index.js
+++ index.js
@@ -1,13 +1,56 @@
 const { Telegraf } = require('telegraf')
 const { message } = require('telegraf/filters')
 const dotenv = require('dotenv')
+const knex = require("./config/db")
 const env = dotenv.config().parsed
 
 const bot = new Telegraf(env.BOT_TOKEN)
 
-bot.start((ctx) => ctx.reply('Welcome'))
+// redis
+const redis = require('redis');
+const client = redis.createClient();
+client.connect();
+
+bot.start(async(ctx) => {
+    const chatId = ctx.chat.id
+    const name   = ctx.chat.first_name
+
+    // register user
+    const hasUser = await knex("users").where({ chat_id: chatId }).first()
+    if(!hasUser)    await knex("users").insert({ chat_id: chatId, name })
+
+    // send welcome message
+    ctx.reply("سلام به ربات لایسنس خوش اومدی:)")
+})
+
+bot.command("admin", async(ctx) => {
+    const chatId = ctx.chat.id
+
+    const user = await knex("users").where({ chat_id: chatId, role: "admin" }).first()
+    if(user){
+        const createAdmin = client.set(`admin:${chatId}`, "verify", { EX: 30 })
+        ctx.reply("با موفقیت وارد شدید✅ \n لطفا پسورد خود را وارد کنید: (30 ثانیه)")
+    }
+}) 
+
+bot.on("text", async(ctx) => {
+    const chatId = ctx.update.message.chat.id
+    const message = ctx.update.message.text
+
+    // get message is password
+    const isPassword = client.get(`admin:${chatId}`)
+    if(isPassword){
+        const validatePassword = await knex("admin_passwords").where({ password: message }).first()
+        if(validatePassword){
+            client.set(`admin:login:${chatId}`, "true", { EX: 604800 })
+            ctx.reply("با موفقیت وارد شدی🚀")
+        }
+        else ctx.reply("پسورد وارد شده صحیح نیست ⛔")
+    }
+})
+
 bot.launch()
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGINT', ()  => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
